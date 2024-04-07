203938247b359d4cd811732ed969279210f289d9
David Michael II <knowdavidmichael@gmail.com>
2024-04-07 10:42:24 +0100

    config: add dependency injection with injectable and get_it

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GromuseApp extends StatelessWidget {
+  const GromuseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
