aa18bb21f8f8f3f784f36c468ed9c687abc0ff3c
David Michael II <knowdavidmichael@gmail.com>
2024-04-21 03:02:09 +0100

    config: add splash screen using `flutter_native_splash` package

--- lib/config/injector/injector.dart
+++ lib/config/injector/injector.dart
@@ -1,13 +1,17 @@
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
 import 'injector.config.dart';
-	
-final di = GetIt.instance;  
-  
-@InjectableInit(  
-  initializerName: 'init',  
-  preferRelativeImports: true,  
-  asExtension: true,  
-)  
-void configureDependencies() => di.init();
+
+final di = GetIt.instance;
+
+@InjectableInit(
+  initializerName: 'init',
+  preferRelativeImports: true,
+  asExtension: true,
+)
+Future<void> configureDependencies() async {
+  await Future.delayed(const Duration(seconds: 2), di.init);
+  FlutterNativeSplash.remove();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'app.dart';
 import 'config/injector/injector.dart';
 
-void main() {
-  configureDependencies();
+Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await configureDependencies();
+  
   runApp(const GromuseApp());
 }
