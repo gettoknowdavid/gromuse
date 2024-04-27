65bb33d47cae7d05522d6131a0c3917f1ff8b99e
David Michael II <knowdavidmichael@gmail.com>
2024-04-27 16:02:00 +0100

    fix: refactor env file to config directory

--- lib/config/database/supabase.dart
+++ lib/config/database/supabase.dart
@@ -1,4 +1,4 @@
-import 'package:gromuse/core/env.dart';
+import 'package:gromuse/config/env.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> configureSupabase() async {
